eb45dd5918ab4ea8457d125e1d637f14f500ea77
RonaldoFidelis <ronaldofidelis.ti@gmail.com>
2024-11-06 13:02:53 -0300

    feat: hook para moduralizar a requisao a api

---
+++ src/service/useLivros.js
@@ -0,0 +1,41 @@
+import { useState, useEffect } from "react"
+
+function useLivros(consulta) {
+	const [livros, setLivros] = useState([]);
+	const [carregamento, setCarregamento] = useState(false);
+	const [erro, setErro] = useState(null);
+
+	useEffect(() => {
+
+		if (!consulta) return;
+
+		const fetchLivros = async () => {
+			setCarregamento(true);
+			setErro(null);
+
+			const chaveApi = import.meta.env.VITE_REACT_APP_API_KEY;
+			const url = `https://www.googleapis.com/books/v1/volumes?q=${consulta}+inauthor&key=${chaveApi}`;
+
+
+			try {
+				const response = await fetch(url);
+				const data = await response.json();
+				setLivros(data.items || []);
+			} catch (e) {
+				setErro(e);
+				console.error(e)
+			} finally {
+				setCarregamento(false);
+			}
+
+		}
+
+		fetchLivros();
+
+	}, [consulta]);
+
+	return { livros, carregamento, erro};
+
+}
+
+export default useLivros
